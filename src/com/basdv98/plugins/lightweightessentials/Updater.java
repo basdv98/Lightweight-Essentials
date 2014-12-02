8366a9930630b9e5ebe80c11c527989855c68a55
Bas de Vet <basdv98@live.nl>
2014-12-02 16:02:23 +0100

    Updated Maven

--- src/com/basdv98/plugins/lightweightessentials/Updater.java
+++ src/com/basdv98/plugins/lightweightessentials/Updater.java
@@ -556,7 +556,6 @@ public class Updater {
 
     private class UpdateRunnable implements Runnable {
 
-        @Override
         public void run() {
             if (Updater.this.url != null) {
                 // Obtain the results of the project's file feed
